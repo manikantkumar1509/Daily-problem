13ee8ea1cf2e90fc3d4fbaa913496ded5be17b10
manikant kumar <97869354+manikantkumar1509@users.noreply.github.com>
2023-09-21 15:29:58 +0530

    basic question 1

---
+++ FindCharacterCase.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+   char c;
+   cin>> c;
+
+   if(c>=65 && c<=90){
+      cout << 1;
+
+   }
+   else if( c>= 97 && c<= 122){
+      cout << 0;
+   }
+   else {
+      cout<< -1;
+   }
+}

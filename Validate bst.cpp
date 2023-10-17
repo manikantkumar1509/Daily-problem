73863782a6d835927f9086ed32d9da1685838e77
manikant kumar <97869354+manikantkumar1509@users.noreply.github.com>
2023-10-17 16:49:03 +0530

    bst (medium)

---
+++ Validate bst.cpp
@@ -0,0 +1,49 @@
+
+    Following is the Binary Tree node structure
+
+    template <typename T>
+    class BinaryTreeNode 
+    {
+      public : 
+        T data;
+        BinaryTreeNode<T> *left;
+        BinaryTreeNode<T> *right;
+
+        BinaryTreeNode(T data) 
+        {
+            this -> data = data;
+            left = NULL;
+            right = NULL;
+        }
+
+        ~BinaryTreeNode() 
+        {
+            if (left)
+            {
+                delete left;
+            }
+            if (right)
+            {
+                delete right;
+            }
+        }
+    };
+
+*************************************************************/
+bool isBST(BinaryTreeNode<int> *root,int min, int max){
+    if(root == NULL){
+        return true;
+    }
+    if(root->data >min && root->data <max){
+        bool left = isBST(root->left, min,root->data);
+        bool right = isBST(root->right,root->data, max);
+          return left && right;
+    }
+    else {
+        return false;
+    }
+}
+bool validateBST(BinaryTreeNode<int>* root) 
+{
+  return isBST(root,INT_MIN, INT_MAX);
+}

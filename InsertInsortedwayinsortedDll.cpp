1aa0c64d86c7fc2a6b6e0f2738659e77fe74cc0d
manikant kumar <97869354+manikantkumar1509@users.noreply.github.com>
2024-10-31 21:42:55 +0530

    gfg problem of the day

---
+++ InsertInsortedwayinsortedDll.cpp
@@ -0,0 +1,38 @@
+class Solution {
+  public:
+    Node* sortedInsert(Node* head, int x) {
+        // Code here
+      Node* newNode = new Node();
+      newNode->data = x;
+      
+      if(!head){
+          return newNode;
+      }
+      
+      if(head->data > x){
+          newNode->next = head;
+          head->prev = newNode;
+          
+          return newNode;
+      }
+      Node* curr = head;
+      Node* temp = NULL;
+      
+      while(curr){
+          if(curr->data <=x){
+              temp = curr;
+          }
+          curr= curr->next;
+      }
+       newNode->next = temp -> next;
+       newNode->prev = temp;
+       temp -> next = newNode;
+       
+       if(newNode->next){
+           newNode->next->prev = newNode;
+       }
+       
+       return head;
+        
+    }
+};
